01ac6d664923ad2e7cc43a0417525df570434779
SayandeepBera <sayandeep4002@gmail.com>
2025-05-17 16:14:36 +0530

    Use bcrypt hashing algorithm for password hashing

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User=require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'Harryisgoodboy';
 
 // Creating a user using POST "/api/auth/createuser". Doesn't require auth
 router.post('/createuser', [
@@ -28,14 +32,27 @@ router.post('/createuser', [
             return res.status(400).json({ error: "A user with this email already exists." });
         }
 
+        // Store your password (hash+salt) formate in DB
+        const salt = await bcrypt.genSalt(10);
+        const securePass = await bcrypt.hash(req.body.password, salt);
+
         // Create new user
         const user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: securePass
         })
         
-        res.json(user);
+        // JWTs enable secure authentication and authorization by providing a way to verify user identity and grant access to specific resources. 
+        const data={
+            user:{
+                id : user.id
+            }
+        }
+        const authToken = jwt.sign(data, JWT_SECRET);
+
+        res.json({authToken});
+        // res.json(user);
 
     }catch{
         console.error(errors.message);
